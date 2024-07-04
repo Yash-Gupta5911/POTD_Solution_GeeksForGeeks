39157cf40aab8558cb1234829097097a0a6b93d0
Yash Gupta <161245313+Yash-Gupta5911@users.noreply.github.com>
2024-07-04 18:05:48 +0530

    Add files via upload

---
+++ 04-07-2024_Duplicate Subtrees.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    string solve(Node *root,unordered_map<string,int> &mp,vector<Node*> &ans){
+        //base case
+        if(!root){
+            return "N";
+        }
+        
+        //recursive case
+        string s=to_string(root->data) + "," + solve(root->left,mp,ans)
+        + "," + solve(root->right,mp,ans);
+        mp[s]++;
+        if(mp[s]==2){
+            ans.push_back(root);
+        }
+        return s;
+    }
+    
+    
+    vector<Node*> printAllDups(Node* root) {
+        unordered_map<string,int> mp;
+        vector<Node*> ans;
+        solve(root,mp,ans);
+        return ans;
+        
+    }
+};
