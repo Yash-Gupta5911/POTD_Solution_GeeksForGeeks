22d38621966a3e4c56b7b1bfbeea29f77841aa84
Yash Gupta <161245313+Yash-Gupta5911@users.noreply.github.com>
2024-07-14 20:09:16 +0530

    Add files via upload

---
+++ 14-07-2024_Segregate 0s and 1s.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        int start =0;
+        int end=arr.size()-1;
+        while(start<end){
+            if(arr[start]==0)
+            start++;
+            else if(arr[end]==1)
+            end--;
+            else{
+                swap(arr[start],arr[end]);
+                start++;
+                end--;
+            }
+        }
+    }
+};
