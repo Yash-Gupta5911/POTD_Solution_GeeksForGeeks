38bd5f1685962122f8ec43f16bdc2b5f525622de
Yash Gupta <161245313+Yash-Gupta5911@users.noreply.github.com>
2024-09-05 17:22:57 +0530

    Add files via upload

---
+++ 05-09-20240_Missing in Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, vector<int>& arr) {
+
+        // Your code goes here
+        long long sum=n*(n+1)/2;
+        long long sum2=0;
+        for(int i=0;i<arr.size();i++){
+            sum2+=arr[i];
+        }
+        return int(sum-sum2);
+    }
+};
