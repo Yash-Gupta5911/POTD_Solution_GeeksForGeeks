0de1e762f99d178680253d94f135332e8d8d216a
Yash Gupta <161245313+Yash-Gupta5911@users.noreply.github.com>
2024-09-12 20:13:59 +0530

    Add files via upload

---
+++ 12-09-20240_Middle of a Linked List.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int getMiddle(Node* head) {
+        Node* slow=head;
+        Node* fast=head;
+        if(!head) return -1;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow->data;
+    }
+};
