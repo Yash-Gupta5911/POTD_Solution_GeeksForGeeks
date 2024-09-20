3a8a5dd4984b75decf9a2630a0eab28a146ae827
Yash Gupta <161245313+Yash-Gupta5911@users.noreply.github.com>
2024-09-20 19:41:29 +0530

    Add files via upload

---
+++ 20-09-20240_Facing the sun.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int countBuildings(vector<int> &height) {
+        int count=1;
+        int maxi=height[0];
+        for(int i=1;i<height.size();i++){
+            if(height[i]>maxi){
+                count++;
+                maxi=height[i];
+            }
+        }
+        return count;
+        
+    }
+};
