af7670085dbeb0314dda7a306c3f9eaba37f293a
Yash Gupta <161245313+Yash-Gupta5911@users.noreply.github.com>
2024-10-16 17:29:18 +0530

    Add files via upload

---
+++ 16-10-2024_Two Swaps.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool checkSorted(vector<int> &arr) {
+        int swaps=0;
+        for(int i=0;i<arr.size();){
+            if(arr[i]!=i+1){
+                swap(arr[i],arr[arr[i]-1]);
+                swaps++;
+            }
+            else i++;
+        }
+        if(swaps==0 || swaps==2) return true;
+        return false;
+    }
+};
